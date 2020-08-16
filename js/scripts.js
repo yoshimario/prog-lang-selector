c81dce43f15b63a486c4b65eb8a435328c997137
Sean Wilson <paintballsean@gmail.com>
2020-08-16 13:36:56 -0700

    Commit addds a header and start of scripts.js file.

---
+++ js/scripts.js
@@ -0,0 +1,9 @@
+$(document).ready(function() {
+  $("#").submit(function() {
+    const number1 = parseInt($("#input1").val());
+    const number2 = parseInt($("#input2").val());
+    const number1 = parseInt($("#input1").val());
+    const number2 = parseInt($("#input2").val());
+    const number2 = parseInt($("#input2").val());
+  });
+});
