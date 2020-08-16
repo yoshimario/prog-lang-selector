4873d5128930f0f21bffe922360064d26a4ba9d0
Sean Wilson <paintballsean@gmail.com>
2020-08-16 16:02:10 -0700

    Commit adds function  one of five questions.

--- js/scripts.js
+++ js/scripts.js
@@ -2,11 +2,11 @@ $(document).ready(function() {
   $("#surveyInfo").submit(function() {
     event.preventDefault();
     // Define variable tallies for responses.
-    let dreamAmmount = 0;
-    let cobaltAmmount= 0;
-    let javaAmmount = 0;
-    let qbasicAmmount = 0;
-    let fortranAmmount = 0;
+    let dreamAmount = 0;
+    let cobaltAmount= 0;
+    let javaAmount = 0;
+    let qbasicAmount = 0;
+    let fortranAmount = 0;
     // Define response variables for question answers.
     const userName = parseInt($("#name").val());
     const responseOne = $("select#questionOne").val();
@@ -15,6 +15,22 @@ $(document).ready(function() {
     const responseFour = $("select#questionFour").val();
     const responseFive = $("select#questionOne").val();
 
+    //Defines functions to make a running tally.
+    if (responseOne === "dream") {
+      dreamAmount = add(dreamAmount, 1);
+    } 
+    else if (responseOne === "fortran") {
+      fortranAmount = add(fortranAmount, 1);
+    }
+    else if (responseOne === "cobalt") {
+      cobaltAmount = add(cobaltAmount, 1);
+    }
+    else if (responseOne === "qbasic") {
+      cobaltAmount = add(cobaltAmount, 1);
+    }
+    else (responseOne === "java") {
+      cobaltAmount = add(cobaltAmount, 1);
+    }
     
   });
 });
