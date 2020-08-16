a17a1ca5049ec7206d6e5997512297bcf80da325
Sean Wilson <paintballsean@gmail.com>
2020-08-16 13:51:42 -0700

    Commit addds five form fields to customize.

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,5 @@
 $(document).ready(function() {
-  $("#").submit(function() {
+  $("#surveyInfo").submit(function() {
     const number1 = parseInt($("#input1").val());
     const number2 = parseInt($("#input2").val());
     const number1 = parseInt($("#input1").val());
