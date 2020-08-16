931ac6b4781ef44623700d728f8d12e4b5cd937d
Sean Wilson <paintballsean@gmail.com>
2020-08-16 14:28:08 -0700

    Commit adds two survey questions.

--- js/scripts.js
+++ js/scripts.js
@@ -1,8 +1,9 @@
 $(document).ready(function() {
   $("#surveyInfo").submit(function() {
-    const number1 = parseInt($("#input1").val());
+    const name = parseInt($("#name").val());
     const number2 = parseInt($("#input2").val());
     const number1 = parseInt($("#input1").val());
+    const number2 = parseInt($("#input2").val());
     const number2 = parseInt($("#input2").val());
     const number2 = parseInt($("#input2").val());
   });
